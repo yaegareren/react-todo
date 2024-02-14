be301f7ac75dcc3ea4c554e263ef2d9b09e13f9f
Shree Mangalam <sshreemangalam@gmail.com>
2024-02-14 11:53:56 +0530

    setup application

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main className="py-10 bg-zinc-100 h-screen">
+        <h1 className="font-extrabold text-2xl text-center">
+          To-do Application using React.js and Typescript
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
