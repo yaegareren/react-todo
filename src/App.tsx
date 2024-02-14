d18d981184ffcb4f759ce4d5978414d220721788
Shree Mangalam <sshreemangalam@gmail.com>
2024-02-14 12:09:09 +0530

    rendering dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { myTodos } from "./data/todos";
+
 function App() {
   return (
     <>
@@ -5,6 +7,15 @@ function App() {
         <h1 className="font-extrabold text-2xl text-center">
           To-do Application using React.js and Typescript
         </h1>
+        <div className="max-w-lg mx-auto">
+          <div className="space-y-2">
+            {myTodos.map((todo) => (
+              <p className="font-normal text-lg" key={todo.id}>
+                {todo.title}
+              </p>
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ src/data/todos.ts
@@ -0,0 +1,29 @@
+import { Todo } from "../types/todo";
+
+export const myTodos: Todo[] = [
+  {
+    id: 1,
+    title: "Learn Graph Data Structures 💻",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Finish LeetCode POTD 💻",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Start canvas project with next.js 💻",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Submit web development assignment 📝",
+    completed: true,
+  },
+  {
+    id: 5,
+    title: "Read class notes for mid semester exams 📓",
+    completed: false,
+  },
+];
